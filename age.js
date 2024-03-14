85323f822ef00c03fc81a4ff87244ee63e6d91b0
mucopivoine <pivotmurerwa1@gmail.com>
2024-03-14 13:07:31 +0200

    age calculator Js

--- age.js
+++ age.js
@@ -1,15 +1,38 @@
- var anyText = document.getElementById("Aging");
- anyText.innerHTML= "";
- const input = document.getElementById("birthdate");
+  var anyText = document.getElementById("Aging");
+  anyText.innerHTML= "";
+  const input = document.getElementById("birthdate");
 
-function calculateAge(){
-    if(input.value === ""){
+ function calculateAge(){
+   
+       const birthdate = new Date(document.getElementById("birthdate").value);
+       const birthYear = birthdate.getFullYear();
+       const thisyear = new Date().getFullYear();
+
+       if(input.value === ""){
         anyText.innerHTML = " Enter your birth date.";
         return;
-    }
-    const birthdate = new Date(document.getElementById("birthdate").value);
-     const bynow = new Date();
-     var difference = bynow.getFullYear() - birthdate.getFullYear();
-      document.getElementById("Aging").innerHTML = `Your age is ${difference} years .`;
+    } else if (isNaN(birthYear)){
+        anyText.innerHTML = "Invalid date entered";
+        return;
+       }else if(birthYear > thisyear){
+        anyText.innerHTML = "No future birth year";
+        return ;
+       } else if(thisyear > birthYear){
+       const difference = thisyear - birthYear;
+      anyText.innerHTML = `Your age is ${difference} years .`;
+      return;
+ }
 }
-document.getElementById("calculate").addEventListener("click", calculateAge);
+ document.getElementById("calculate").addEventListener("click", calculateAge);
+ 
+ input.addEventListener("input",function(){
+    const enterDate = new Date(input.value);
+    const enterYear = enterDate.getFullYear();
+
+    if(enterYear.toString().length > 4){
+        input.value = input.value.slice(0, -1);
+    }
+ });
+
+
+
