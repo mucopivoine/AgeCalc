10e73e090595c81761bbb8f7905f40dfdc8101ce
mucopivoine <pivotmurerwa1@gmail.com>
2024-03-14 11:16:37 +0200

    age calculation javascript

--- age.js
+++ age.js
@@ -1,7 +1,12 @@
  var anyText = document.getElementById("Aging");
  anyText.innerHTML= "";
+ const input = document.getElementById("birthdate");
 
 function calculateAge(){
+    if(input.value === ""){
+        anyText.innerHTML = " Enter your birth date";
+        return;
+    }
     const birthdate = new Date(document.getElementById("birthdate").value);
      const bynow = new Date();
      var difference = bynow.getFullYear() - birthdate.getFullYear();
